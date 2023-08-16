5df38e352ad051901c297d19a60c60a2bc88632f
OyugoObonyo <nyikal1999@gmail.com>
2023-08-16 16:09:45 +0300

    update user context

--- lib/betting_project/accounts/account.ex
+++ lib/betting_project/accounts/account.ex
@@ -4,10 +4,8 @@ defmodule BettingProject.Accounts.Account do
 
   alias BettingProject.Repo
   alias BettingProject.Users.User
-  alias BettingProject.Plans.Plan
 
   schema "accounts" do
-    belongs_to(:plan, Plan, foreign_key: :plan_id)
     belongs_to(:user, User, foreign_key: :user_id)
     timestamps()
   end
@@ -15,7 +13,6 @@ defmodule BettingProject.Accounts.Account do
   def changeset(struct, params) do
     struct
     |> cast(params, [
-      :plan_id,
       :user_id
     ])
   end
