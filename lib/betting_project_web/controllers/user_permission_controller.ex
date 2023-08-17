075e91cd194f99c8ce6a10fa55dee67deb707117
OyugoObonyo <nyikal1999@gmail.com>
2023-08-17 16:21:30 +0300

    Complete setting up permissions

--- lib/betting_project_web/controllers/user_permission_controller.ex
+++ lib/betting_project_web/controllers/user_permission_controller.ex
@@ -2,10 +2,16 @@ defmodule BettingProjectWeb.UserPermissionController do
   use BettingProjectWeb, :controller
   alias BettingProjectWeb.UsersPermissions.UserPermission
   alias BettingProjectWeb.ErrorResponse
+  alias BettingProjectWeb.Auth.CheckPermission
 
   action_fallback(BettingProjectWeb.FallbackController)
 
   def add_user_permission(conn, %{"id" => user_id, "permission_id" => permission_id}) do
+    user = conn.assigns[:user]
+    required_permission = "canAssignPermission"
+    user_access_status = CheckPermission.can_user_do?(user, required_permission)
+    if user_access_status != true, do: raise(ErrorResponse.Forbidden)
+
     with {:ok, _new_user_permission} <-
            UserPermission.create(%{"user_id" => user_id, "permission_id" => permission_id}) do
       conn
@@ -15,6 +21,11 @@ defmodule BettingProjectWeb.UserPermissionController do
   end
 
   def delete_user_permission(conn, %{"id" => user_id, "permission_id" => permission_id}) do
+    user = conn.assigns[:user]
+    required_permission = "canRevokePermission"
+    user_access_status = CheckPermission.can_user_do?(user, required_permission)
+    if user_access_status != true, do: raise(ErrorResponse.Forbidden)
+
     with user_permission = %UserPermission{} <-
            UserPermission.find_user_permission(user_id, permission_id),
          {_, _} <-
@@ -23,7 +34,7 @@ defmodule BettingProjectWeb.UserPermissionController do
       |> put_status(:ok)
       |> json(%{"message" => "User permission revoked successfully"})
     else
-      _ ->
+      nil ->
         raise ErrorResponse.NotFound
     end
   end
