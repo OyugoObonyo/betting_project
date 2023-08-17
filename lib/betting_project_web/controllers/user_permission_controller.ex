b7901cc592b625ea4e7599c734ce6c116c064782
OyugoObonyo <nyikal1999@gmail.com>
2023-08-17 12:22:41 +0300

    Alias user permission context

--- lib/betting_project_web/controllers/user_permission_controller.ex
+++ lib/betting_project_web/controllers/user_permission_controller.ex
@@ -1,6 +1,6 @@
 defmodule BettingProjectWeb.UserPermissionController do
   use BettingProjectWeb, :controller
-  alias BettingProject.UsersPermissions.UserPermission
+  alias BettingProjectWeb.UsersPermissions.UserPermission
   alias BettingProjectWeb.ErrorResponse
 
   action_fallback(EmailsProjectWeb.FallbackController)
