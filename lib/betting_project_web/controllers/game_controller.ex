8651157c16a1c77e3273a55b04e6f58f293a53a4
OyugoObonyo <nyikal1999@gmail.com>
2023-08-18 05:09:34 +0300

    Add create action in game controller

--- lib/betting_project_web/controllers/game_controller.ex
+++ lib/betting_project_web/controllers/game_controller.ex
@@ -1,10 +1,17 @@
 defmodule BettingProjectWeb.GameController do
   use BettingProjectWeb, :controller
   alias BettingProject.Games.Game
+  alias BettingProjectWeb.ErrorResponse
+  alias BettingProjectWeb.Auth.CheckPermission
 
-  action_fallback(BettingProject.FallbackController)
+  action_fallback(BettingProjectWeb.FallbackController)
 
-  def add_game(conn, params) do
+  def create(conn, params) do
+    user = conn.assigns[:user]
+    required_permission = "canAddGame"
+    user_access_status = CheckPermission.can_user_do?(user, required_permission)
+    if user_access_status != true, do: raise(ErrorResponse.Forbidden)
+
     with {:ok, game} <- Game.create(params) do
       conn
       |> put_status(:created)
