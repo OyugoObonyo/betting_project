7b2387dabcc45fc6dbfb42d9f056e7f2e964f392
OyugoObonyo <nyikal1999@gmail.com>
2023-08-18 10:51:45 +0300

    Validate status values are valid

--- lib/betting_project/bets/bet.ex
+++ lib/betting_project/bets/bet.ex
@@ -1,4 +1,5 @@
 defmodule BettingProject.Bets.Bet do
+  import Ecto.Query
   import Ecto.Changeset
   use Ecto.Schema
 
@@ -17,20 +18,27 @@ defmodule BettingProject.Bets.Bet do
   def changeset(struct, params) do
     struct
     |> cast(params, [
+      :user_id,
+      :game_id,
       :amount,
       :status
     ])
     |> validate_required([:amount, :user_id, :game_id])
-    |> validate_inclusion(:status, ["P", "W", "D"])
+    |> validate_inclusion(:status, ["P", "W", "L"])
     |> foreign_key_constraint(:user_id)
     |> foreign_key_constraint(:game_id)
   end
 
   def create(params) do
-    IO.inspect(params, label: "INCOMING CREATE BET PARAMS!")
-
     %__MODULE__{}
     |> changeset(params)
     |> Repo.insert()
+  end
+
+  def find_game_profit_loss(game_id) do
+    wins_query = from(g in __MODULE__, where: g.game_id == ^game_id and g.status == "W")
+    loss_query = from(g in __MODULE__, where: g.game_id == ^game_id and g.status == "L")
+    Repo.aggregate(loss_query, :sum, :amount) - Repo.aggregate(wins_query, :sum, :amount)
+    |>
   end
 end
